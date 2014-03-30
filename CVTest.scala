4a5fc5ebdfc731807b01dbc61988d4cb6dd800d8
Jacob Van De Weert <vandeweertj@gmail.com>
2014-03-29 23:34:47 -0400

    abstract display code to handle other block/cell sizes

--- CVTest.scala
+++ CVTest.scala
@@ -1,7 +1,7 @@
 import java.awt.image.BufferedImage
 import java.io.ByteArrayInputStream
 import javax.imageio.ImageIO
-import javax.swing.{ImageIcon, JFrame, JLabel}
+import javax.swing.{ImageIcon, JFrame, JLabel, WindowConstants}
 import org.opencv.core.{Core, Mat, MatOfByte, MatOfFloat, MatOfPoint, Point, Size, Scalar}
 import org.opencv.highgui.Highgui
 import org.opencv.imgproc.Imgproc
@@ -10,9 +10,34 @@ import scala.math.{cos, sin, toRadians}
 
 object CVTest {
 
-  def displayBuffImage (bufImg: BufferedImage) : Unit = {
-    
-    val frame = new JFrame("Test Image")
+  def computeAndDisplayHOG (img: Mat) (winSize: Size, blockSize: Size,
+                                       blockStride: Size, cellSize: Size,
+                                       numBins: Int) (visScaler: Float) (windowTitle: String) : Unit = {
+
+    val colorimg = new Mat
+    Imgproc.cvtColor(img, colorimg, Imgproc.COLOR_GRAY2RGB)
+
+    val hog = new HOGDescriptor(winSize, blockSize, blockStride, cellSize, numBins)
+    val descVals = new MatOfFloat
+    val locs = new MatOfPoint
+    hog.compute(img, descVals, new Size(0, 0), new Size(0, 0), locs)
+
+    val hogimg = getHOGDescriptorVisual(colorimg)(descVals.toArray)(winSize,
+                                        blockSize, blockStride, cellSize, numBins)(visScaler)
+    val byteMat = new MatOfByte
+    Highgui.imencode(".png", hogimg, byteMat)
+
+    val bytes = byteMat.toArray
+    val in = new ByteArrayInputStream(bytes)
+    val bufImg = ImageIO.read(in)
+
+    displayBuffImage(bufImg)(windowTitle)
+
+  }
+
+  def displayBuffImage (bufImg: BufferedImage) (windowTitle: String) : Unit = {
+
+    val frame = new JFrame(windowTitle)
     val label = new JLabel
     val icon = new ImageIcon
 
@@ -21,7 +46,7 @@ object CVTest {
 
     frame.getContentPane.add(label)
     frame.setResizable(false)
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+    frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE)
     frame.pack
     frame.setVisible(true)
 
@@ -29,43 +54,43 @@ object CVTest {
 
   // Assistance with visualization algorithm found here:
   // http://www.juergenwiki.de/work/wiki/doku.php?id=public%3ahog_descriptor_computation_and_visualization
-  def getHOGDescriptorVisual (image: Mat)
-                             (descVals: Array[Float], winSize: Size,
-                                 cellSize: Size, numBins: Int)
+  def getHOGDescriptorVisual (image: Mat) (descVals: Array[Float])
+                             (winSize: Size, blockSize: Size,
+                              blockStride: Size, cellSize: Size, numBins: Int)
                              (visScaler: Float) : Mat = {
 
     val img = image.clone
     val binRads = toRadians(180.0 / numBins)
-    val cellsInXDir = winSize.width / cellSize.width
-    val cellsInYDir = winSize.height / cellSize.height
-    val numCells = cellsInXDir * cellsInYDir
+    val cellsInXDir = (winSize.width / cellSize.width).toInt
+    val cellsInYDir = (winSize.height / cellSize.height).toInt
+    val cellsInBlockX = (blockSize.width / cellSize.width).toInt
+    val cellsInBlockY = (blockSize.height / cellSize.height).toInt
 
     val gradientStrengths = Array.ofDim[Float](cellsInYDir, cellsInXDir, numBins)
     val cellUpdateCounter = Array.ofDim[Int](cellsInYDir, cellsInXDir)
 
-    val blocksInXDir = cellsInXDir - 1
-    val blocksInYDir = cellsInYDir - 1
+    val blocksInXDir = (winSize.width/blockStride.width - (blockSize.width-blockStride.width) / blockStride.width).toInt
+    val blocksInYDir = (winSize.height/blockStride.height - (blockSize.height-blockStride.height) / blockStride.height).toInt
     var descDataIdx = 0
 
     // sum gradient strengths for each cell
     for (blockx <- 0 until blocksInXDir) {
       for (blocky <- 0 until blocksInYDir) {
-        for (cellNum <- 0 until 4) {
+        for(celly <- 0 until cellsInBlockY) {
+          for (cellx <- 0 until cellsInBlockX) {
 
-          val (cellx, celly) = cellNum match {
-            case 0 => (blockx, blocky)
-            case 1 => (blockx, blocky + 1)
-            case 2 => (blockx + 1, blocky)
-            case 3 => (blockx + 1, blocky + 1)
+            val (prevx, prevy) = ((blockx*blockStride.width / cellSize.width).toInt,
+                                  (blocky*blockStride.height / cellSize.height).toInt)
+            val (indx, indy) = (prevx + cellx, prevy + celly)
+
+            for (bin <- 0 until numBins) {
+              gradientStrengths(indy)(indx)(bin) += descVals(descDataIdx)
+              descDataIdx += 1
+            }
+
+            cellUpdateCounter(indy)(indx) += 1
+
           }
-
-          for (bin <- 0 until numBins) {
-            gradientStrengths(celly)(cellx)(bin) += descVals(descDataIdx)
-            descDataIdx += 1
-          }
-
-          cellUpdateCounter(celly)(cellx) += 1
-
         }
       }
     }
@@ -103,8 +128,8 @@ object CVTest {
             val dirVecX = cos(currRad)
             val dirVecY = sin(currRad)
             val maxVecLen = cellSize.width / 2
-            val mx = drawX + cellSize.width/2
-            val my = drawY + cellSize.height/2
+            val mx = tlX + cellSize.width/2
+            val my = tlY + cellSize.height/2
             val xLen = dirVecX*gradStrength*maxVecLen*visScaler
             val yLen = dirVecY*gradStrength*maxVecLen*visScaler
 
@@ -127,32 +152,15 @@ object CVTest {
 
     System.loadLibrary(Core.NATIVE_LIBRARY_NAME)
 
-    val byteMat = new MatOfByte
+    val img = Highgui.imread("resources/lena.png", Highgui.CV_LOAD_IMAGE_GRAYSCALE)
 
-    val img = Highgui.imread("resources/lena.png",
-                             Highgui.CV_LOAD_IMAGE_GRAYSCALE)
-    val colorimg = new Mat
-    Imgproc.cvtColor(img, colorimg, Imgproc.COLOR_GRAY2RGB)
+    var winSize = new Size(512, 512) // size of lena
+    var blockSize = new Size(64, 64)
+    var blockStride = new Size(32, 32)
+    var cellSize = new Size(32, 32)
+    var numBins = 9
 
-    val winSize = new Size(512, 512) // size of lena
-    val blockSize = new Size(64, 64)
-    val blockStride = new Size(32, 32)
-    val cellSize = new Size(32, 32)
-    val numBins = 9
-
-    val hog = new HOGDescriptor(winSize, blockSize, blockStride, cellSize, numBins)
-    val descVals = new MatOfFloat
-    val locs = new MatOfPoint
-    hog.compute(img, descVals, new Size(0, 0), new Size(0, 0), locs)
-
-    val hogimg = getHOGDescriptorVisual(colorimg)(descVals.toArray)(winSize)(cellSize)(3.0f)
-    Highgui.imencode(".png", hogimg, byteMat)
-    
-    val bytes = byteMat.toArray
-    val in = new ByteArrayInputStream(bytes)
-    val bufImg = ImageIO.read(in)
-
-    displayBuffImage(bufImg)
+    computeAndDisplayHOG(img)(winSize, blockSize, blockStride, cellSize, numBins)(2.5f)("Test Image")
 
   }
 }
