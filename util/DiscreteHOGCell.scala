fe0f04f340835fc2d6c1ed79786937c21c6c392b
Jacob Van De Weert <vandeweertj@gmail.com>
2014-04-04 22:56:47 -0400

    touchups and converter from continuous to discrete HOG cells

--- util/DiscreteHOGCell.scala
+++ util/DiscreteHOGCell.scala
@@ -1,5 +1,16 @@
 package imgdetect.util
 
+/* Companion class for DiscreteHOGCell */
+object DiscreteHOGCell {
+
+  // convert a cell of continuous HOG gradients to a DiscreteHOGCell
+  // TODO(jacob) I believe it's technically possible for floating point errors
+  //     to cause (g * numParts).toInt == numParts, an error condition
+  def discretizeHOGCell (grads: Array[Float], numParts: Int) : DiscreteHOGCell =
+    new DiscreteHOGCell(grads.length, numParts, grads.map(g => (g * numParts).toInt))
+
+}
+
 /* A helper class to represent a discrete version of a HOG descriptor cell.
  * numBins: the number of gradient bins for this HOG cell
  * numPartitions: the number of bins in each gradient bin. The total number
@@ -14,7 +25,7 @@ class DiscreteHOGCell (val numBins: Int, val numPartitions: Int, val gradients: 
   def this (numBins: Int, numPartitions: Int) = this(numBins, numPartitions, new Array[Int](numBins))
 
 
-  override def equals (other: Any) = other match {
+  override def equals (other: Any) : Boolean = other match {
 
     case hogObj: DiscreteHOGCell => numBins == hogObj.numBins &&
                                     numPartitions == hogObj.numPartitions &&
